d3debeed4f30558dbf45a4543fc55490847d0beb
Erin Taketa <erin.ktaketa@gmail.com>
2025-04-01 17:41:55 -0600

    Add set up files with home and color page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [HeaderComponent, RouterOutlet],
+  template: `
+    <app-header></app-header>
+    <router-outlet></router-outlet>
+`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
---
+++ src/app/color/color.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-color',
+  imports: [RouterModule],
+  templateUrl: './color.component.html',
+  styleUrl: './color.component.css'
+})
+export class ColorComponent {
+
+}
