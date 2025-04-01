82a22f795b35b040f06ae5b51c85aee3a61aaffe
Brenna Wolf <brennarw@colostate.edu>
2025-04-01 17:44:26 -0600

    about page started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ProfileCardComponent } from './about-page/profile-card/profile-card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AboutPageComponent, ProfileCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+// @Component({
+//   selector: 'app-about-page',
+//   imports: [AboutPageComponent],
+//   templateUrl: './app/about-page/about-page.component.html',
+//   styleUrl: './app/about-page/about-page.component.css'
+// })
+
 export class AppComponent {
   title = 'cs312-group-project';
 }
