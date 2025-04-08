c39016f444a5b0a0dfd9da66f28a34e8e1627722
Hannah <hannahss@colostate.edu>
2025-04-07 18:58:13 -0600

    table 1 for color has been completed!

--- src/app/color/color.component.spec.ts
+++ src/app/color/color.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ColorComponent } from './color.component';
 
 describe('ColorComponent', () => {
--- src/app/color/color.component.ts
+++ src/app/color/color.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-color',
-  imports: [RouterModule],
+  standalone: true, // 
+  imports: [CommonModule, RouterModule], 
   templateUrl: './color.component.html',
   styleUrl: './color.component.css'
 })
 export class ColorComponent {
+  numRows: number = 22;
+  numCols: number = 41; 
+  rowsArray: number[] = Array(this.numRows).fill(0);
+  columnLabels: string[] = this.generateColumnLabels(this.numCols);
 
+  generateColumnLabels(count: number): string[] {
+    const labels: string[] = [];
+    for (let i = 0; i < count; i++) {
+      let label = '';
+      let temp = i;
+      do {
+        label = String.fromCharCode((temp % 26) + 65) + label;
+        temp = Math.floor(temp / 26) - 1;
+      } while (temp >= 0);
+      labels.push(label);
+    }
+    return labels;
+  }
+  onCellClick(col: string, row: number) {
+    alert(`${col}${row}`);
+  }
 }
