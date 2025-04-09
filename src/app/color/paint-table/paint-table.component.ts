d820eae44c7b8384e308134fe82f22fe9cbf1e54
daja-w <104468175+daja-w@users.noreply.github.com>
2025-04-09 17:46:24 -0600

    getters for row & column labels

--- src/app/color/paint-table/paint-table.component.ts
+++ src/app/color/paint-table/paint-table.component.ts
@@ -1,13 +1,37 @@
 import { Component, Input } from "@angular/core";
-
+import { CommonModule } from "@angular/common";
 @Component({
     selector: 'paint-table',
     standalone: true,
     templateUrl: './paint-table.component.html',
-    styleUrl: './paint-table.component.css'
+    styleUrl: './paint-table.component.css',
+    imports: [CommonModule]
 })
 
 export class PaintTableComponent {
     @Input() numRows!: number;
     @Input() numCols!: number;
+
+    get columns(): string[] {
+        const labels: string[] = [];
+
+        for (let i = 0; i < this.numCols; i++) {
+            let label = '';
+            let n = i;
+            do {
+                // convert number to letter based on ascii
+                label = String.fromCharCode((n % 26) + 65) + label;
+                // get consecutive letters if needed
+                n = Math.floor(n / 26) - 1;
+            } while (n >= 0);
+            labels.push(label);
+        }
+        return labels;
+    }
+
+    get rows(): number[] {
+        return Array.from({ length: this.numRows }, (_, i) => i + 1);
+    }
+
+      
 }
